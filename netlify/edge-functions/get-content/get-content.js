8a9ff6792c8ec07b4154661a5b896fa9b3ac7d73
andystevenson <andystevenson@mac.com>
2022-09-13 10:37:34 +0100

    weird header rules

--- netlify/edge-functions/get-content/get-content.js
+++ netlify/edge-functions/get-content/get-content.js
@@ -1,15 +1,19 @@
 export default async (request, { log }) => {
   const url = new URL(request.url)
   const content = `${url.origin}/.netlify/functions/content`
-  try {
-    const data = await fetch(content, { headers: { authorization: 'hey up' } })
-    if (data.ok) {
-      const text = await data.text()
-      return new Response(`fetched [${text}]`)
-    } else {
-      throw Error(`fetch-content failed ${data.status}`)
-    }
-  } catch (error) {
-    log(`fetch-content error! [${error.message}]`)
-  }
+  await content
+  // try {
+  //   const data = await fetch(content, { headers: { authorization: 'hey up' } })
+  //   if (data.ok) {
+  //     const text = await data.text()
+  //     return new Response(`fetched [${text}]`)
+  //   } else {
+  //     throw Error(`fetch-content failed ${data.status}`)
+  //   }
+  // } catch (error) {
+  //   log(`fetch-content error! [${error.message}]`)
+  // }
+  return new Response('Hello, World!', {
+    headers: { 'content-type': 'text/html' },
+  })
 }
