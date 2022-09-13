c374e61ddf11c7ef139318366162eddd060aa40c
andystevenson <andystevenson@mac.com>
2022-09-13 10:43:16 +0100

    weirdness!

--- netlify/edge-functions/get-content/get-content.js
+++ netlify/edge-functions/get-content/get-content.js
@@ -1,19 +1,15 @@
 export default async (request, { log }) => {
   const url = new URL(request.url)
   const content = `${url.origin}/.netlify/functions/content`
-  await content
-  // try {
-  //   const data = await fetch(content, { headers: { authorization: 'hey up' } })
-  //   if (data.ok) {
-  //     const text = await data.text()
-  //     return new Response(`fetched [${text}]`)
-  //   } else {
-  //     throw Error(`fetch-content failed ${data.status}`)
-  //   }
-  // } catch (error) {
-  //   log(`fetch-content error! [${error.message}]`)
-  // }
-  return new Response('Hello, World!', {
-    headers: { 'content-type': 'text/html' },
-  })
+  try {
+    const data = await fetch(content, { headers: { authorization: 'hey up' } })
+    if (data.ok) {
+      const text = await data.text()
+      return new Response(`fetched [${text}]`)
+    } else {
+      throw Error(`fetch-content failed ${data.status}`)
+    }
+  } catch (error) {
+    log(`fetch-content error! [${error.message}]`)
+  }
 }
