e3ad35d836a05fa65023ea814f80dda43f2d597c
andystevenson <andystevenson@mac.com>
2022-09-13 09:39:01 +0100

    added a ttl?

--- netlify/functions/on-demand-content/on-demand-content.js
+++ netlify/functions/on-demand-content/on-demand-content.js
@@ -1,8 +1,10 @@
 const { builder } = require('@netlify/functions')
 async function handler() {
+  const date = new Date()
   return {
     statusCode: 200,
-    body: 'on-demand-content content content',
+    body: `on-demand-content @${date.toLocaleTimeString()}`,
+    ttl: 120,
   }
 }
 
